558ffe6a0b00675304502443ac818a4f6f7a2b80
Thomas Bennett <tbennett@bennett-services.com>
2022-02-23 08:09:14 -0500

    completed

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,12 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
-    if let Err(e) = minigrep::read_file(config) {
-        println!("Application error: {}", e);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
